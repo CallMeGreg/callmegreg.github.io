64de6240b14eeb22ac188f25242ca62adfaa1261
callmegreg <callmegreg@github.com>
2022-12-27 11:07:08 -0500

    added test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// write a test case that always passes:
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/big things coming soon.../i);
+  expect(linkElement).toBeInTheDocument();
+});
