3799e14e060200c66554057b03a9c3dcfb8459d5
Greg Mohler <callmegreg@github.com>
2026-06-21 13:32:45 -0400

    bigger margin on mobile

--- src/HouseAlwaysWins.jsx
+++ src/HouseAlwaysWins.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import './HouseAlwaysWins.css';
 
 function HouseAlwaysWins() {
   return (
-    <div className="house-always-wins" style={{ height: '100vh', width: '100vw', paddingBottom: '20px', boxSizing: 'border-box' }}>
+    <div className="house-always-wins">
       <iframe
         src="/house-always-wins/house-always-wins.html"
-        style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
         allowFullScreen
         title="The House Always Wins"
       ></iframe>
